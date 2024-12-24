2eeeaadabad3764cb484c380a785a23a7c9b05b2
mirmidonec <62998936+mirmidonec@users.noreply.github.com>
2024-12-24 21:44:51 +0300

    Add files via upload

---
+++ snake/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-New Unity Project-1.0.15";
+const contentToCache = [
+    "Build/snake.loader.js",
+    "Build/snake.framework.js.unityweb",
+    "Build/snake.data.unityweb",
+    "Build/snake.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
